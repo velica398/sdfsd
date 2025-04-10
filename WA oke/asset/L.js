5362abb316f00e926ffb2f5cb98bfe0c29cb13c8
velica398 <c02122843@gmail.com>
2025-04-11 05:56:11 +0700

    Delete WA oke directory

--- WA oke/asset/L.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('L').addEventListener('click', function () {
-    const url = 'https://wa.me/6281234567890';
-    window.open(url, '_self');
-  });
--- WA oke/redirect.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function() {
-    window.location.href = "#";
-  }, 2500);
